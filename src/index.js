0564ef56c54b64c29142b05ff089e49a01cbd10a
Mischel <susan.mwaura@student.moringaschool.com>
2023-06-29 09:32:02 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // Get the form element
+const form = document.querySelector('form');
+
+// Add event listener for the submit event
+form.addEventListener('submit', function(event) {
+  // Prevent the default behavior of the submit event
+  event.preventDefault();
+
+  // Additional code to handle the submit event
+  // ...
+  
+});
 });
